26cf66b40778164dbf30f954213f6d1eb17a1870
Mahesh Kunchala <maheshkunchala18@gmail.com>
2025-05-23 00:41:54 +0530

    Update data-source

--- data-source.js
+++ data-source.js
@@ -6,12 +6,12 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT, 10) || 5432,
-  username: process.env.DB_USERNAME || "postgres",
-  password: process.env.DB_PASSWORD || "postgressql",
-  database: process.env.DB_NAME || "User_Access_Management",
-  synchronize: true, // set false in production & use migrations
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: true,
   logging: false,
   entities: [
     "./models/**/*.js",
